fd8a5f9ec06d78c3ad84fd3aaf8dc934d24e25d0
Gabriel-Eduardo-Ruffo <gon.near@gmail.com>
2021-01-05 08:45:47 -0300

    arreglo path 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 console.log('Corriendo NODE');
 
 /*PATH estaticos PUBLIC*/
-const publicPath = path.resolve (__dirname, '../public');
+const publicPath = path.resolve (__dirname, 'public');
 app.use (express.static(publicPath));
 
 app.listen(process.env.PORT||3000,()=>{console.log("servidor funcionando")});
